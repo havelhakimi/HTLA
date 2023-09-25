680b82c31269f2c515eab850ac368aeeaad4e7d7
havelhakimi <33575438+havelhakimi@users.noreply.github.com>
2023-09-25 16:56:38 +0530

    Update train.py

--- train.py
+++ train.py
@@ -89,7 +89,7 @@ parser.add_argument('--contrast', default=1, type=int, help='Whether use contras
 parser.add_argument('--graph', default=0, type=int, help='Whether use graph encoder.')
 parser.add_argument('--layer', default=1, type=int, help='Layer of Graphormer.')
 parser.add_argument('--mod_type', default='bert-base-uncased', type=str, choices=['bert-base-uncased','roberta-large'], help='Select backbone')
-parser.add_argument('--graph_type', type=str, default='graphormer', help='graph type')
+parser.add_argument('--graph_type', type=str, default='GCN', help='graph type')
 parser.add_argument('--label_refiner', default=1, type=int, help='Label Refiner for Garphormer.')
 parser.add_argument('--bce_wt', type=float, default=1, help='bce_wt.')
 parser.add_argument('--dot', default=0, type=int, help='Dot prod.')
